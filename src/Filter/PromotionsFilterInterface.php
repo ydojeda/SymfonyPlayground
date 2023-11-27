6370905a6015f84dc6b68abcdfc871d44b89724d
ydojeda <yna.ojeda@asknice.ly>
2023-11-28 10:46:56 +1100

    LowestPriceEnquiry | set up data transfer objects

---
+++ src/Filter/PromotionsFilterInterface.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Filter;
+
+use App\DTO\PromotionEnquiryInterface;
+
+interface PromotionsFilterInterface
+{
+    public function apply(PromotionEnquiryInterface $enquiry): PromotionEnquiryInterface;
+
+}
