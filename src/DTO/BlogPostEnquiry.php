e2625fcf2f6fc28b60fe13724843bdc81fbccf18
ydojeda <yna.ojeda@asknice.ly>
2023-12-04 08:41:47 +1300

    add DTO for blog post create/update enquiries

---
+++ src/DTO/BlogPostEnquiry.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\DTO;
+
+use http\Message\Body;
+
+class BlogPostEnquiry
+{
+    private ?int $userId;
+
+    private ?string $tags = null;
+
+    private ?string $body = null;
+
+    private ?int $reactions = null;
+
+    public function getUserId(): ?int
+    {
+        return $this->userId;
+    }
+
+    public function setUserId(?int $userId): void
+    {
+        $this->userId = $userId;
+    }
+
+    public function getTags(): ?string
+    {
+        return $this->tags;
+    }
+
+    public function setTags(?string $tags): void
+    {
+        $this->tags = $tags;
+    }
+
+    public function getBody(): ?string
+    {
+        return $this->body;
+    }
+
+    public function setBody(?string $body): void
+    {
+        $this->body = $body;
+    }
+
+    public function getReactions(): ?int
+    {
+        return $this->reactions;
+    }
+
+    public function setReactions(?int $reactions): void
+    {
+        $this->reactions = $reactions;
+    }
+
+
+}
